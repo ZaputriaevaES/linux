43878a06134ce4caebb7c740fda9349e22db75be
ZaputriaevaES <zaputriaeva.es@phystech.edu>
2025-05-04 10:41:20 +0000

    add new sbi call in system call

--- arch/riscv/kernel/riscv_regtbl.c
+++ arch/riscv/kernel/riscv_regtbl.c
@@ -2,13 +2,17 @@
 #include <linux/syscalls.h>
 #include <asm/ptrace.h>
 #include <asm/csr.h>
+#include <asm/sbi.h>
+
+// Extension id-number of my sbi-call
+#define SBI_EXT_CSR_DUMP 0x43535244  // "CSRD" in ASCII
 
 SYSCALL_DEFINE0(riscv_regtbl) 
 {
     struct pt_regs *regs = current_pt_regs();
 
-    // Таблица GP-регистров
-    printk(KERN_INFO "General Purpose Registers:");
+    // GP-register table
+    printk(KERN_INFO "General Purpose Registers (GP):");
     printk(KERN_INFO "Register | Hex Value          | Dec Value");
     printk(KERN_INFO "---------|--------------------|------------");
     printk(KERN_INFO "ra       | 0x%016lx | %ld", regs->ra, regs->ra);
@@ -18,15 +22,31 @@ SYSCALL_DEFINE0(riscv_regtbl)
     printk(KERN_INFO "a0       | 0x%016lx | %ld", regs->a0, regs->a0);
     printk(KERN_INFO "a1       | 0x%016lx | %ld", regs->a1, regs->a1);
     printk(KERN_INFO "---------|--------------------|------------");
-
-    // // Таблица CSR-регистров
-    // printk(KERN_INFO "\nControl/Status Registers (CSR):");
-    // printk(KERN_INFO "CSR      | Hex Value    | Description");
-    // printk(KERN_INFO "---------|-------------|----------------");
-    // printk(KERN_INFO "mstatus  | 0x%08lx   | Machine status", csr_read(CSR_MSTATUS));
-    // printk(KERN_INFO "mepc     | 0x%08lx   | Exception PC", csr_read(CSR_MEPC));
-    // printk(KERN_INFO "mtval    | 0x%08lx   | Trap value", csr_read(CSR_MTVAL));
-    // printk(KERN_INFO "mhartid  | 0x%08lx   | Hardware thread ID", csr_read(CSR_MHARTID));
     
+    unsigned long mstatus = sbi_ecall(SBI_EXT_CSR_DUMP, 0, CSR_MSTATUS, 0, 0, 0, 0, 0).value;
+    unsigned long mepc    = sbi_ecall(SBI_EXT_CSR_DUMP, 0, CSR_MEPC, 0, 0, 0, 0, 0).value;
+    unsigned long mtval   = sbi_ecall(SBI_EXT_CSR_DUMP, 0, CSR_MTVAL, 0, 0, 0, 0, 0).value;
+    unsigned long mhartid = sbi_ecall(SBI_EXT_CSR_DUMP, 0, CSR_MHARTID, 0, 0, 0, 0, 0).value;
+    
+    // int mstatus_error = sbi_ecall(SBI_EXT_CSR_DUMP, 0, CSR_MSTATUS, 0, 0, 0, 0, 0).error;
+    // int mepc_error    = sbi_ecall(SBI_EXT_CSR_DUMP, 0, CSR_MEPC, 0, 0, 0, 0, 0).error;
+    // int mtval_error   = sbi_ecall(SBI_EXT_CSR_DUMP, 0, CSR_MTVAL, 0, 0, 0, 0, 0).error;
+    // int mhartid_error = sbi_ecall(SBI_EXT_CSR_DUMP, 0, CSR_MHARTID, 0, 0, 0, 0, 0).error;
+    // printk("SBI error code: %d\n", mstatus_error);
+    // printk("SBI error code: %d\n", mepc_error);
+    // printk("SBI error code: %d\n", mtval_error);
+    // printk("SBI error code: %d\n", mhartid_error);
+    
+    // CSR-register table
+    printk(KERN_INFO "Control/Status Registers (CSR):");
+    printk(KERN_INFO "Register | Hex Value          | Dec Value");
+    printk(KERN_INFO "---------|--------------------|------------");
+    printk(KERN_INFO "mstatus  | 0x%016lx | %ld", mstatus, mstatus);
+    printk(KERN_INFO "mepc     | 0x%016lx | %ld", mepc, mepc);
+    printk(KERN_INFO "mtval    | 0x%016lx | %ld", mtval, mtval);
+    printk(KERN_INFO "mhartid  | 0x%016lx | %ld", mhartid, mhartid);
+    printk(KERN_INFO "---------|--------------------|------------");
+    printk(KERN_INFO "---------|--------------------|------------");
+
     return 0;
 }
